6b303a895b22237727426453bdb4a6238a359646
David Hayes <drhayes@gmail.com>
2022-02-26 09:35:56 -0600

    Whoops, change game name in conf.

--- src/conf.lua
+++ src/conf.lua
@@ -1,7 +1,7 @@
 local config = require 'gameConfig'
 
 function love.conf(t)
-  t.window.title = 'Gemini Rising'
+  t.window.title = 'Aftershock'
   t.window.icon = 'media/images/icon.png'
   t.window.width = config.graphics.width * config.graphics.gameScale
   t.window.height = config.graphics.height * config.graphics.gameScale
@@ -10,7 +10,7 @@ function love.conf(t)
   t.window.resizable = true
   t.window.vsync = 1
 
-  t.identity = 'gemini-rising'
+  t.identity = 'aftershock'
   t.version = '11.4'
   t.modules.physics = false
   t.modules.touch = false
